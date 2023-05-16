56e95bd41224318ba83e9ae1cd53ae71059ab797
lbcanfield <lewie.b.canfield@gmail.com>
2023-05-16 06:32:35 -0500

    Inital Commit - Installed bootstrap dependency - imported useState and useEffect - Created a slice of state for acquired data from mock api - created a useEffect hook to handle api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+
+const url = 'https://swapi.dev/api/people/';
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+     // Try to think through what state you'll need for this app before starting. Then build out
+     const [characters, setCharacters] = useState([]);
+     // the state properties here.
+     useEffect(() => {
+          axios.get(url)
+               .then(response => {
+                    console.log(response.data);
+               })
+               .catch(error => {
+                    console.error(error);
+               })
+     }, [])
+     // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+     // side effect in a component, you want to think about which state and/or props it should
+     // sync up with, if any.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+     return (
+          <div className="App">
+               <h1 className="Header">Characters</h1>
+          </div>
+     );
 }
 
 export default App;
